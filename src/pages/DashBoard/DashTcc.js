db9211056bfa24ad59948bb9a1204e3c11837881
Pedro Sousa <pompermayerpedrohenrique@gmail.com>
2024-09-08 12:13:56 -0300

    feat: implementação da tela de dashboard de tcc's e salas

---
+++ src/pages/DashBoard/DashTcc.js
@@ -0,0 +1,50 @@
+import NavBar from '../../components/NavBar'
+import '../../assets/css/DashBoard/Dashboard.css'
+
+function Dashb_Tcc() {
+
+    return (
+        <div >
+            <NavBar />
+            <div class="container-sm">
+                <div class="name">
+                    <h3>DashBoard TCC's</h3>
+                </div>
+                <div class="pesquisa ">
+                    <input type="text" class="form-control" placeholder="Buscar Registros"/>
+                    <button type="button" class="btn btn-danger">Pesquisar</button>
+                </div>
+                <div>
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">CURSO</th>
+                            <th scope="col">TÍTULO</th>
+                            <th scope="col">TEMA</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                    </tbody>
+                </table>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Dashb_Tcc;
